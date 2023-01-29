80b2eee76f76de4342fdc1438e5311f8164509f8
lucasRosssi <lucasrm2305@gmail.com>
2023-01-29 20:27:30 -0300

    feat: get movement offset yaw for anim blending

--- Source/MonsterShooter/ShooterAnimInstance.cpp
+++ Source/MonsterShooter/ShooterAnimInstance.cpp
@@ -4,6 +4,7 @@
 #include "ShooterAnimInstance.h"
 #include "ShooterCharacter.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
 
 void UShooterAnimInstance::NativeInitializeAnimation()
 {
@@ -39,4 +40,7 @@ void UShooterAnimInstance::UpdateAnimationProperties(float DeltaTime)
     bIsAccelerating = false;
   }
 
+  FRotator AimRotation = ShooterCharacter->GetBaseAimRotation();
+  FRotator MovementRotation = UKismetMathLibrary::MakeRotFromX(ShooterCharacter->GetVelocity());
+  MovementOffsetYaw = UKismetMathLibrary::NormalizedDeltaRotator(MovementRotation, AimRotation).Yaw;
 }
--- Source/MonsterShooter/ShooterAnimInstance.h
+++ Source/MonsterShooter/ShooterAnimInstance.h
@@ -35,4 +35,8 @@ private:
   // Whether or not the character is moving
   UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
   bool bIsAccelerating;
+
+  // Offset yaw used for strafing
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Movement, meta = (AllowPrivateAccess = "true"))
+  float MovementOffsetYaw;
 };
