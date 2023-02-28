588d4689e4659665635c23261f23954ebdee72d5
lucasRosssi <lucasrm2305@gmail.com>
2023-02-27 21:00:06 -0300

    feat: set last offset yaw

--- Source/MonsterShooter/ShooterAnimInstance.cpp
+++ Source/MonsterShooter/ShooterAnimInstance.cpp
@@ -43,4 +43,9 @@ void UShooterAnimInstance::UpdateAnimationProperties(float DeltaTime)
   FRotator AimRotation = ShooterCharacter->GetBaseAimRotation();
   FRotator MovementRotation = UKismetMathLibrary::MakeRotFromX(ShooterCharacter->GetVelocity());
   MovementOffsetYaw = UKismetMathLibrary::NormalizedDeltaRotator(MovementRotation, AimRotation).Yaw;
+
+  if (ShooterCharacter->GetVelocity().Size() > 0.f)
+  {
+    LastMovementOffsetYaw = MovementOffsetYaw;
+  }
 }
